36d27408768b0c77cb152df0dbdde32397d66ebf
Ankit787762 <ankitsawami6@gmail.com>
2026-06-03 14:23:41 +0530

    Refactor input fields for expense and budget in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,23 @@ function App() {
       <h1 className="text-3xl font-bold text-center">
         Expense Tracker
       </h1>
-      <div className="ml-50 mt-20 ">
+      <div className="mx-auto mt-20 w-50">
       <h2 className="font-bold ">
         Balance:
       </h2>
       <div className="flex gap-100 mt-3">
-        <p>Expense <input type="number" placeholder="enter expense"/></p>
-        <p>Budget <input type="number" placeholder="enter Budget"/></p>
+        <div className="flex gap-2">
+        <p>Expense</p>
+        <input className="border px-2 py-1 w-50" type="number" placeholder="enter expense"/>
+        </div>
+        <div className="flex gap-2">
+        <p>Expense</p>
+        <input className="border px-2 py-1 w-50" type="number" placeholder="enter expense"/>
+        </div>
       </div>
-      <div className="mt-3">
+      <div className="mt-3 ">
         <h1 className="font-bold">Transaction</h1>
+        <input className="border px-2 py-1 w-full" type="text" placeholder="search expenses" />
       </div>
       </div>
       
